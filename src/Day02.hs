36a18b811ab8b017c1a35a568ca890d6ee8d730c
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2015-12-03 19:50:15 +0000

    Day 2

---
+++ src/Day02.hs
@@ -0,0 +1,24 @@
+module Day02 (answer1, answer2) where
+
+import Control.Monad (liftM)
+import Data.List.Split (splitOn)
+import Data.List (sort)
+
+answer1 :: IO Int
+answer1 = do
+  dimensions <- getData
+  return $ sum (map giftWrap dimensions)
+
+
+answer2 = do
+  dimensions <- getData
+  return $ sum (map giftRibbon dimensions)
+
+getData = do
+  raw <- liftM lines (readFile "./data/02.txt")
+  let dims = [sort $ map read (splitOn "x" l) | l <- raw] :: [[Int]]
+  return dims
+
+giftWrap (l:w:h:_) = l*w + 2 * (l*w + w*h + h*l)
+
+giftRibbon (l:w:h:_) = 2 * (l+w) + l*w*h
--- src/Main.hs
+++ src/Main.hs
@@ -3,11 +3,14 @@ module Main where
 import System.Environment (getArgs)
 import Control.Monad (liftM)
 import qualified Day01 as D01
+import qualified Day02 as D02
 
 main :: IO ()
 main = do
-  (day : pbNumber : rest) <- getArgs >>= return . map read :: IO ([Int])
+  (day : pbNumber : rest) <- liftM (map read) getArgs :: IO [Int]
   case day * 10 + pbNumber of
     11 -> D01.answer1 >>= print
     12 -> D01.answer2 >>= print
+    21 -> D02.answer1 >>= print
+    22 -> D02.answer2 >>= print
     _ -> print "not done yet"
