dc0c047eed101be585ffcd4b9d5c8d1abecf97e2
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2015-12-25 11:49:39 +0000

    tail rec version

--- src/Day25.hs
+++ src/Day25.hs
@@ -3,7 +3,8 @@ module Day25 (answer1) where
 answer1 :: Int
 answer1 = code 3010 3019
 
-code 1 1 = 20151125
-code i j = let
-  prev = if j == 1 then code 1 (i-1) else code (i+1) (j-1)
-  in (252533 * prev) `mod` 33554393
+code i j = go i j 20151125
+  where go 1 1 acc = acc
+        go i j acc = let
+          (i', j') = if j == 1 then (1, i-1) else (i+1, j-1)
+          in go i' j' ((acc * 252533) `mod` 33554393)
