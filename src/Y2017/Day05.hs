113ac22f6894127cc97c8ffca309430c14c1768a
Grégoire Charvet 黑瓜 <greg+git@geekingfrog.com>
2017-12-05 19:58:49 +0000

    Reduce code duplication

--- src/Y2017/Day05.hs
+++ src/Y2017/Day05.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Y2017.Day05 (answer1, answer2) where
 
+import Control.Monad.Primitive
 import Control.Monad.ST.Strict
 import qualified Data.Vector.Mutable as MV
 
@@ -14,33 +17,36 @@ answer2 = do
 
 countJumps :: [Int] -> Int
 countJumps jumps = runST $ do
-    let n = length jumps
-    js <- MV.new n
-    mapM_ (\(v, i) -> MV.write js i v) (zip jumps [0 ..])
-    loop js 0 0
-
-loop js i k = if k >= MV.length js
-    then pure i
-    else do
-        j <- MV.read js k
-        MV.modify js (+1)    k
-        loop      js (i + 1) (k + j)
-
+    js <- initStack jumps
+    loop js (const (+1)) 0 0
 
 countJumps2 :: [Int] -> Int
 countJumps2 jumps = runST $ do
+    js <- initStack jumps
+    let f v = if v >= 3 then subtract 1 else (+1)
+    loop js f 0 0
+
+initStack :: [a] -> (forall s . ST s (MV.MVector s a))
+initStack jumps = do
     let n = length jumps
     js <- MV.new n
     mapM_ (\(v, i) -> MV.write js i v) (zip jumps [0 ..])
-    loop2 js 0 0
+    pure js
 
-loop2 js i k = if k >= MV.length js
+
+loop
+    :: (PrimMonad m)
+    => MV.MVector (PrimState m) Int
+    -> (Int -> Int -> Int)
+    -> Int
+    -> Int
+    -> m Int
+loop js f i k = if k >= MV.length js
     then pure i
     else do
         j <- MV.read js k
-        let d = if j >= 3 then subtract 1 else (+1)
-        MV.modify js d       k
-        loop2     js (i + 1) (k + j)
+        MV.modify js (f j) k
+        loop js f (i + 1) (k + j)
 
 testJumps :: [Int]
 testJumps = [0, 3, 0, 1, -3]
