70ecdc2e4b006177505bed045aa769147e5e46f9
Grégoire Charvet 黑瓜 <greg+git@geekingfrog.com>
2018-05-04 17:14:00 +0100

    Use zippers

--- src/Y2017/Day25.hs
+++ src/Y2017/Day25.hs
@@ -2,16 +2,15 @@ module Y2017.Day25
   ( answer1
   ) where
 
-import Data.Vector as V
 import Data.Foldable as F
-import qualified Data.Vector.Mutable as MV
-import Control.Monad.ST
-import Data.Functor
 
 answer1 :: Int
-answer1 =
-    let vEnd = runMut
-    in V.length $ V.filter id vEnd
+answer1 = runTape
+
+data Tape a = Tape [a] a [a]
+
+instance (Show a) => Show (Tape a) where
+    show (Tape xs e ys) = unwords (map show $ reverse xs) ++ " [" ++ show e ++ "] " ++ unwords (map show ys)
 
 data State
     = A
@@ -22,29 +21,40 @@ data State
     | F
      deriving (Show)
 
-runMut =
-    runST $
-    do let n = 12964419
-       -- arbitratry size for the tape, too big and the copying time
-       -- gets really big, too small and I get out of bounds exc
-       tape <- MV.replicate 50000 False
-       foldM'_ (\x _ -> stepMut tape x) (25000, A) (V.fromList [1 .. n])
-       V.unsafeFreeze tape
+runTape =
+    let n = 12964419
+        (finalTape, finalState) = F.foldl' (\(t, s) _ -> stepTape t s)
+                                           (Tape [] False [], A)
+                                           [1 .. n]
+        tapes = iterate (uncurry stepTape) (Tape [] False [], A)
+    in  countTape True finalTape
 
-stepMut tape (i, s) = do
-    v <- MV.read tape i
-    (i', s') <- 
-        case (s, v) of
-            (A, False) -> MV.write tape i True $> (i + 1, B)
-            (A, True) -> MV.write tape i False $> (i + 1, F)
-            (B, False) -> MV.write tape i False $> (i - 1, B)
-            (B, True) -> MV.write tape i True $> (i - 1, C)
-            (C, False) -> MV.write tape i True $> (i - 1, D)
-            (C, True) -> MV.write tape i False $> (i + 1, C)
-            (D, False) -> MV.write tape i True $> (i - 1, E)
-            (D, True) -> MV.write tape i True $> (i + 1, A)
-            (E, False) -> MV.write tape i True $> (i - 1, F)
-            (E, True) -> MV.write tape i False $> (i - 1, D)
-            (F, False) -> MV.write tape i True $> (i + 1, A)
-            (F, True) -> MV.write tape i False $> (i - 1, E)
-    pure (i', s')
+write :: Tape a -> a -> Tape a
+write (Tape xs _ ys) x = Tape xs x ys
+
+right, left :: Tape Bool -> Tape Bool
+right (Tape xs e []) = Tape (e:xs) False []
+right (Tape xs e (y:ys)) = Tape (e:xs) y ys
+left (Tape [] e ys) = Tape [] False (e:ys)
+left (Tape (x:xs) e ys) = Tape xs x (e:ys)
+
+countTape :: Eq a => a -> Tape a -> Int
+countTape k (Tape xs e ys) =
+    Prelude.length (Prelude.filter (==k) xs)
+        + Prelude.length (Prelude.filter (==k) ys)
+        + if k == e then 1 else 0
+
+stepTape :: Tape Bool -> State -> (Tape Bool, State)
+stepTape t@(Tape _ e _) s = case (s, e) of
+    (A, False) -> (right $ write t True, B)
+    (A, True ) -> (right $ write t False, F)
+    (B, False) -> (left $ write t False, B)
+    (B, True ) -> (left $ write t True, C)
+    (C, False) -> (left $ write t True, D)
+    (C, True ) -> (right $ write t False, C)
+    (D, False) -> (left $ write t True, E)
+    (D, True ) -> (right $ write t True, A)
+    (E, False) -> (left $ write t True, F)
+    (E, True ) -> (left $ write t False, D)
+    (F, False) -> (right $ write t True, A)
+    (F, True ) -> (left $ write t False, E)
